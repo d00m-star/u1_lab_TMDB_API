d5d3f8c955fea292d1358edb4ab5897dae043bd9
John Dunn <alch3myst@gmail.com>
2022-07-01 21:04:10 -0500

    TMDB HW First Commit

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-const API_KEY = ''
+const API_KEY = '22702854f3a7f9662ad7778bcffca72e'
 const DOMAIN = 'https://api.themoviedb.org/3'
 const IMAGE_BASE_PATH = 'https://image.tmdb.org/t/p/original'
+
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+const section = document.querySelector('section')
+
+const renderList = (movies) => {
+  input.value = ''
+  movies.forEach((movie) => {
+    let newDiv = document.createElement('div')
+    let newH2 = document.createElement('h2')
+    newH2.innerText = movie.title
+    newDiv.innerHTML = `<img src =${IMAGE_BASE_PATH + movie.poster_path} />`
+    section.appendChild(newDiv)
+    newDiv.appendChild(newH2)
+  })
+}
+
+const getMovies = async () => {
+  let search = input.value
+  let response = await axios.get(
+    `${DOMAIN}/search/movie?query=${search}&api_key=${API_KEY}`
+  )
+  renderList(response.data.results)
+}
+
+button.addEventListener('click', getMovies)
+
+/* if movie poster is clicked, show movie details at top of page */
